015ee9d6853b2a5ded3cc2dc4575fd7fd97e84bb
Esteban <etejedaw@hotmail.com>
2026-04-15 10:47:50 -0400

    chore: add interval tracker in matomo

--- public/matomo.js
+++ public/matomo.js
@@ -1,14 +1,15 @@
 var _paq = (window._paq = window._paq || []);
+_paq.push(["enableHeartBeatTimer", 15]);
 _paq.push(["trackPageView"]);
 _paq.push(["enableLinkTracking"]);
 (function () {
-  var u = "https://matomo.tebita.xyz/";
-  _paq.push(["setTrackerUrl", u + "matomo.php"]);
-  _paq.push(["setSiteId", "3"]);
-  var d = document,
-    g = d.createElement("script"),
-    s = d.getElementsByTagName("script")[0];
-  g.async = true;
-  g.src = u + "matomo.js";
-  s.parentNode.insertBefore(g, s);
+	var u = "https://matomo.tebita.xyz/";
+	_paq.push(["setTrackerUrl", u + "matomo.php"]);
+	_paq.push(["setSiteId", "3"]);
+	var d = document,
+		g = d.createElement("script"),
+		s = d.getElementsByTagName("script")[0];
+	g.async = true;
+	g.src = u + "matomo.js";
+	s.parentNode.insertBefore(g, s);
 })();
