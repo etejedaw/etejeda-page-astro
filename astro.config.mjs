4882a84c6b1cc7153b87457807d976a203cf414a
Esteban <etejedaw@hotmail.com>
2026-04-22 14:16:55 -0400

    feat: add 404 page, robots.txt and sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://etejeda.dev"
+	site: "https://etejeda.dev",
+	integrations: [sitemap()]
 });
